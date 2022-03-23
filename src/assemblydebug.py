99b0827dd7b0c7a3ba8e03bc36fb736856d8eac3
Tom Gouville <tom.gouville@protonmail.com>
2022-03-23 16:26:41 +0100

    Verify if target file exists

--- src/assemblydebug.py
+++ src/assemblydebug.py
@@ -10,26 +10,22 @@ def print_registers(register_set):
     child: SBValue
     for child in register_set:
         print('\tName: ', child.GetName(), ' Value: ', child.GetValue())
+    
 
+if __name__ == "__main__":
+    prog_name = "a.out"
+    if len(sys.argv) > 1:
+        prog_name = sys.argv[-1]
 
-prog_name = "a.out"
-if len(sys.argv) > 1:
-    prog_name = sys.argv[-1]
+    
+    dbg = lldb.SBDebugger().Create()
+    dbg.SetAsync (False)
+    target : SBTarget = dbg.CreateTarget(prog_name)
 
-dbg = lldb.SBDebugger().Create()
-dbg.SetAsync (False)
+    if not target:
+        print(f"'{prog_name}' not found.")
+        exit(1)
 
-print(f"Debugging {prog_name}")
-target : SBTarget = dbg.CreateTarget(prog_name)
-
-if target:
-    # bp = target.EvaluateExpression("break set -p return -X main")
-    #bp = target.BreakpointCreateByLocation("main.c", 7)
-    # bp = target.BreakpointCreateByRegex("return")
-    strlist = SBStringList()
-    strlist.AppendString("main")
-    # bp = target.BreakpointCreateBySourceRegex("return", SBFileSpecList(), SBFileSpecList(), strlist)
-    # bp = target.BreakpointCreateByName("breakpoint")
     print("\n>>>>>>> SETTING BREAKPOINT <<<<<<<<")
     bp = target.BreakpointCreateByRegex("breakpoint")
     print(f"breakpoint : {bp}")
@@ -47,16 +43,17 @@ if target:
         frame = thread.GetFrameAtIndex (0)
         print(f"frame: {frame}")
 
+        
+        print("\n>>>>>>> REGISTERS DUMP <<<<<<<<")
+
         registerList = frame.GetRegisters()
         print('Frame registers (size of register set = %d):' % registerList.GetSize())
         # for value in registerList:
         #     print_registers(value)
-        print("\n>>>>>>> REGISTERS DUMP <<<<<<<<")
-        print_registers(registerList[0]) # Registres generaux
+        # Printing General Purpose Registers
+        print_registers(registerList[0])
         process.Continue()
         
         print("\n>>>>>>> STDOUT <<<<<<<<")
-        mystr = process.GetSTDOUT(100000)
-        print(mystr)
-
-# val3 = target.EvaluateExpression("register read --format u")
+        stdout_data = process.GetSTDOUT(100000)
+        print(stdout_data)
